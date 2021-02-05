d153d8f9294681044a3c3848ff24dca6dcf8282c
malikimansaparov <malikimansaparov@gmail.com>
2021-02-05 12:10:48 +0600

    refactoring code

--- src/App.js
+++ src/App.js
@@ -19,17 +19,17 @@ class App extends Component {
 
     gettingWeather = async (e) =>{
         e.preventDefault()
-        var city = e.target.elements.city.value;
+        const city = e.target.elements.city.value;
 
         if(city){
             const api_url = await
                 fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
             const data = await api_url.json();
 
-            var sunset = data.sys.sunset;
-            var date = new Date();
+            const sunset = data.sys.sunset;
+            const date = new Date();
             date.setTime(sunset);
-            var sunset_date = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+            const sunset_date = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
 
             this.setState({
                 temp:data.main.temp,
